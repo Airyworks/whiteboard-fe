41cf902830205d72bcdf860aab4e877b2af5952b
Matt <you1474600@gmail.com>
2017-11-04 14:01:02 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <nav id="nav">
+      <img id="logo" src="./assets/logo.jpg">
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+#nav
+  height: 70px
+  width: 100%
+  background-color: #fff
+  border-bottom: 1px solid #ddd
+  box-shadow: #ddd 0px 3px 3px
+  text-align: center
+  margin-bottom: 15px
+  & #logo
+    width: 50px
+    height: 50px
+    margin: 10px
+  
+</style>
